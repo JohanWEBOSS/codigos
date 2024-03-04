ade2e4655c21b6e2b64d77e4a2551d9312fe0f14
JohanWEBOSS <139427955+JohanWEBOSS@users.noreply.github.com>
2024-03-04 10:43:52 -0700

    Add files via upload

--- extraer.py
+++ extraer.py
@@ -1,7 +1,7 @@
 from  bs4 import BeautifulSoup
 import requests
 
-website = 'https://www.homedepot.com.mx/pinturas/acabados-para-superficies?storeId=10351'
+website = 'https://www.soriana.com/?msclkid=211da0ebcf8618c8af0ac50dc8405dc8&utm_source=bing&utm_medium=cpc&utm_campaign=bing_performance_web_search_conversion_nacional_brand&utm_term=www+soriana+com&utm_content=Brand_General_.com'
 results = requests.get(website)
 content = results.text
 
